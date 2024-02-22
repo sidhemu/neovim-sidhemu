166bd5f3b4d1e0c15043c75751804e660d828ad0
Siddharth Hembrom <sidhemu2017@gmail.com>
2024-02-22 23:55:33 +0100

    few more setting for bufferline and nvim tree

--- lua/sidhemu/plugins/bufferline.lua
+++ lua/sidhemu/plugins/bufferline.lua
@@ -6,14 +6,33 @@ end
 
 -- enable bufferline
 buffer_line.setup({
+	highlights = require("catppuccin.groups.integrations.bufferline").get(),
 	options = {
 		mode = "buffers",
-		indicator_icon = "▎",
-		buffer_close_icon = "",
-		-- buffer_close_icon = '',
+		numbers = "ordinal",
+		left_mouse_command = "buffer %d",
+		indicator = {
+			icon = "▎",
+			style = "icon",
+		},
+		-- indicator_icon = "▎",
+		-- buffer_close_icon = "",
+		buffer_close_icon = "󰅙",
 		modified_icon = "●",
-		close_icon = "",
+		close_icon = "󰅙",
 		-- close_icon = '',
+		show_tab_indicators = true,
+		always_show_bufferline = true,
+		enforce_regular_tabs = false,
+		view = "multiwindow",
+		show_buffer_close_icons = true,
+		themable = true,
+		color_icons = true,
+		hover = {
+			enabled = true,
+			delay = 200,
+			reveal = { "close" },
+		},
 		offsets = {
 			{
 				filetype = "NvimTree",
